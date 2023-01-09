1929dcee75fc4bb5e94156cd0fc5e9f8a237177b
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-01-10 00:36:48 +0200

    linement

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,18 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[form-modal-open]"),
+      closeModalBtn: document.querySelector("[form-modal-close]"),
+      modal: document.querySelector("[form-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
