857324cb7e656c2a934f0e482f3db2f36ac58df5
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-01-11 14:59:00 +0200

    reprocessed modal

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,18 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-    });
+  const refs = {
+    openModalBtn: document.querySelector("[data-open-menu]"),
+    closeModalBtn: document.querySelector("[data-close-menu]"),
+    modal: document.querySelector("[data-menu-container]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", testModal);
+  function testModal() {
+    console.log("click occured")
+  }
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-closed");
+  }
   })();
